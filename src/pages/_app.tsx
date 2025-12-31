81ff6a87b8aba8795ee9daf18e1e75d5083f18c2
Leogom26 <leogomecommerce@gmail.com>
2025-12-31 18:17:16 -0300

    correcao do helmet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,29 +3,33 @@ import type { AppProps } from "next/app";
 import { Header } from "../components/header";
 import { SessionProvider } from "next-auth/react";
 
-// Adicione estas duas linhas
+// Importações do Toastify
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+// Adicione o Provider do Helmet para compatibilidade com React 19 em 2026
+import { HelmetProvider } from "react-helmet-async";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
+    <HelmetProvider>
+      <SessionProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
 
-      {/* Adicione o ToastContainer aqui para que funcione em todo o app */}
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </SessionProvider>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </SessionProvider>
+    </HelmetProvider>
   );
 }
---
+++ src/pages/politicadeuso/politicadeuso.tsx
@@ -0,0 +1,3 @@
+export default function PoliticaDeUso() {
+  return <div>Sua política aqui...</div>;
+}
